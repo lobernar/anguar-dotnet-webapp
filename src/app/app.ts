bb9aa462c989a3c654cdd667dfccaf372d355aa9
Loic Bernard <loic.bernard@ulb.be>
2025-11-11 23:19:35 +0100

    Removed default template and imported authentication component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthenticationComponent } from './authentication-component/authentication-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AuthenticationComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
